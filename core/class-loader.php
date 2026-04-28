980ed2b9d1704503c019ab36f18768dc5a4cbce5
nkgarg21b <nkgarg21@gmail.com>
2026-04-28 11:30:14 +0530

    Phase 3.2: Secure handler hooks

--- core/class-loader.php
+++ core/class-loader.php
@@ -11,6 +11,9 @@ class HYIP_Loader {
         require_once HYIP_PLUGIN_PATH . 'includes/class-transactions.php';
         require_once HYIP_PLUGIN_PATH . 'includes/class-plans.php';
         require_once HYIP_PLUGIN_PATH . 'includes/class-investments.php';
+        require_once HYIP_PLUGIN_PATH . 'includes/class-deposits.php';
+        require_once HYIP_PLUGIN_PATH . 'includes/class-razorpay.php';
+        require_once HYIP_PLUGIN_PATH . 'includes/class-payment-handler.php';
         require_once HYIP_PLUGIN_PATH . 'admin/class-admin-menu.php';
         require_once HYIP_PLUGIN_PATH . 'public/class-public.php';
     }
@@ -23,6 +26,9 @@ class HYIP_Loader {
             wp_schedule_event(time(), 'daily', 'hyip_daily_roi');
         }
         add_action('hyip_daily_roi', ['HYIP_Investments', 'process_daily_roi']);
+
+        add_action('admin_post_nopriv_hyip_payment_callback', ['HYIP_Payment_Handler', 'handle_callback']);
+        add_action('admin_post_hyip_payment_callback', ['HYIP_Payment_Handler', 'handle_callback']);
 
         new HYIP_Public();
     }
