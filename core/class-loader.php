d8bd024cc8f20f5a97a7b5c16e48c4ca257d0253
nkgarg21b <nkgarg21@gmail.com>
2026-04-28 10:39:31 +0530

    Phase 0: Add loader class

---
+++ core/class-loader.php
@@ -0,0 +1,16 @@
+<?php
+class HYIP_Loader {
+    public function run() {
+        $this->load_dependencies();
+        $this->init_hooks();
+    }
+
+    private function load_dependencies() {
+        require_once HYIP_PLUGIN_PATH . 'includes/database.php';
+        require_once HYIP_PLUGIN_PATH . 'admin/class-admin-menu.php';
+    }
+
+    private function init_hooks() {
+        add_action('admin_menu', ['HYIP_Admin_Menu', 'register_menu']);
+    }
+}
